84cb2a46615e5e095121c799f785869c173c86f6
tasmirz <zihad2107071@stud.kuet.ac.bd>
2026-04-21 21:28:46 +0600

    dd

--- Torrent_stream/Torrent_stream/APIService.swift
+++ Torrent_stream/Torrent_stream/APIService.swift
@@ -270,12 +270,12 @@ final class APIService {
             throw APIError.invalidURL
         }
 
-        try await withCheckedThrowingContinuation { continuation in
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
             socket.send(.string(text)) { error in
                 if let error {
                     continuation.resume(throwing: error)
                 } else {
-                    continuation.resume()
+                    continuation.resume(returning: ())
                 }
             }
         }
